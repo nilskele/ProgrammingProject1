a698c8878db178fc275cd328a972d1ba48d8ee19
gustberchmans <gustberchmans@gmail.com>
2024-03-13 12:02:49 +0100

    Niks nuttig

--- Medialab/src/Main.java
+++ Medialab/src/Main.java
@@ -5,6 +5,7 @@ public class Main {
         //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
         // to see how IntelliJ IDEA suggests fixing it.
         System.out.printf("Hello and welcome!");
+        System.out.println("Hallo terug!");
 
     }
 }
