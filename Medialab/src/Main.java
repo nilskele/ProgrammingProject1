12deaab82884f16da122e4f2cdf36cfba35229ab
nils.kelecom <kelecom.nils@gmail.com>
2024-03-27 09:42:48 +0100

    creation of java module and main java file

--- Medialab/src/Main.java
+++ Medialab/src/Main.java
@@ -4,7 +4,7 @@ public class Main {
     public static void main(String[] args) {
         //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
         // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        System.out.print("Hello and welcome!");
 
     }
 }
