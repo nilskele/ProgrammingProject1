c32a8f1d3d0eac271811ae7e91bcd61da444db45
nilskele <145758921+nilskele@users.noreply.github.com>
2024-04-12 13:46:59 +0000

    vue en bootstrap environment

---
+++ demo/src/main.js
@@ -0,0 +1,9 @@
+import './assets/main.css'
+
+import { createApp } from 'vue'
+import App from './App.vue'
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap/dist/js/bootstrap.js'
+
+
+createApp(App).mount('#app')
