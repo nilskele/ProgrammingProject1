cdb8b89d08932c9986dced95343553a101ec6544
gustberchmans <gustberchmans@gmail.com>
2024-04-16 14:46:12 +0200

    Update index.php

--- php/index.php
+++ php/index.php
@@ -41,35 +41,12 @@
       <div class = "Div3D" >
         <img src = "../images/3d-printer_44218.png" height = "400px" alt = "3d" />
       </div>
-<<<<<<< Updated upstream
-      <a class = "btn btn-primary adminbtn" href = "./admin.php">
-        Admin
-      </a>
-      <div class = "loginForm">
-        <div class = "form-group row">
-          <label for = "inputEmail3" class = "col-sm-2 col-form-label">
-            Email:
-          </label>
-          <div class = "col-sm-10">
-            <input type = "email" class = "form-control" id = "inputEmail3" placeholder = "Email" />
-          </div>
-
-        </div>
-        <div class = "form-group row">
-          <label for = "inputName3" class = "col-sm-2 col-form-label">
-            Naam:
-          </label>
-
-          <div class = "col-sm-10">
-            <input type = "text" class = "form-control" id = "inputName3" placeholder = "Naam" />
-=======
       <a class="btn btn-primary adminbtn" href="admin.php">Admin</a>
       <div class="loginForm">
         <div class="form-group row">
           <label for="inputEmail3" class="col-sm-2 col-form-label">Email:</label>
           <div class="col-sm-10">
             <input type="email" class="form-control" id="inputEmail3" placeholder="Email" />
->>>>>>> Stashed changes
           </div>
         </div>
 
