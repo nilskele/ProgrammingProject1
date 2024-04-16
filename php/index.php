b463231467037eda425b605eb9049a5878a70a4c
gustberchmans <gustberchmans@gmail.com>
2024-04-16 14:22:24 +0200

    clean code!!!

--- php/index.php
+++ php/index.php
@@ -1,23 +1,23 @@
-<!DOCTYPE html>
-<html lang="en">
+<!DOCTYPE html >
+<html lang = "en">
 
 <head>
-  <meta charset="UTF-8" />
-  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <meta charset = "UTF-8" />
+  <meta name = "viewport" content = "width=device-width, initial-scale=1.0" />
   <title>Medialab</title>
-  <link rel="stylesheet" href="../bootstrap/css/bootstrap.min.css" />
-  <link rel="stylesheet" href="../css/styles.css" />
+  <link rel = "stylesheet" href = "../bootstrap/css/bootstrap.min.css" />
+  <link rel = "stylesheet" href = "../css/styles.css" />
 </head>
 
 <body>
 
-  <div id="app">
-    <div id="container">
+  <div id = "app">
+    <div id = "container">
     <?php
-  include('header.php');
-  ?>
-      <div class="welcomeDiv">
-        <div class="welcome">
+      include('header.php');
+    ?>
+      <div class = "welcomeDiv">
+        <div class = "welcome">
           <h1>
             Welcome <br />
             in het Medialab
@@ -27,46 +27,67 @@
             hebt gelezen!! Meld uzelf hieronder aan of maak <br />
             een account met u school mail.
           </p>
-          <div class="WelcomeButtons">
-            <button class="btn btn-primary" href="student.php">Account aanmaken</button>
-            <button class="btn btn-light" href="/catalogus">Log in</button>
+          <div class = "WelcomeButtons">
+            <button class = "btn btn-primary" href = "student.php">Account aanmaken</button>
+            <button class = "btn btn-light" href = "/catalogus">Log in</button>
           </div>
         </div>
       </div>
-      <div class="Div3D">
-        <img src="../images/3d-printer_44218.png" height="400px" alt="3d" />
+
+      <div class = "Div3D" >
+        <img src = "../images/3d-printer_44218.png" height = "400px" alt = "3d" />
       </div>
-      <button class="btn btn-primary adminbtn" href="/admin">Admin</button>
-      <div class="loginForm">
-        <div class="form-group row">
-          <label for="inputEmail3" class="col-sm-2 col-form-label">Email:</label>
-          <div class="col-sm-10">
-            <input type="email" class="form-control" id="inputEmail3" placeholder="Email" />
+
+      <button class = "btn btn-primary adminbtn" href = "/admin">
+        Admin
+      </button>
+
+      <div class = "loginForm">
+        <div class = "form-group row">
+
+          <label for = "inputEmail3" class="col-sm-2 col-form-label">
+            Email:
+          </label>
+
+          <div class = "col-sm-10">
+            <input type = "email" class = "form-control" id = "inputEmail3" placeholder = "Email" />
+          </div>
+
+        </div>
+        <div class = "form-group row">
+          <label for = "inputName3" class = "col-sm-2 col-form-label">
+            Naam:
+          </label>
+
+          <div class = "col-sm-10">
+            <input type = "text" class = "form-control" id = "inputName3" placeholder = "Naam" />
           </div>
         </div>
-        <div class="form-group row">
-          <label for="inputName3" class="col-sm-2 col-form-label">Naam:</label>
-          <div class="col-sm-10">
-            <input type="text" class="form-control" id="inputName3" placeholder="Naam" />
+
+        <div class = "form-group row">
+          <label for = "inputPassword3" class = "col-sm-2 col-form-label">
+            Password:
+          </label>
+          
+          <div class = "col-sm-10">
+            <input type = "password" class = "form-control" id = "inputPassword3" placeholder = "Password" />
           </div>
         </div>
-        <div class="form-group row">
-          <label for="inputPassword3" class="col-sm-2 col-form-label">Password:</label>
-          <div class="col-sm-10">
-            <input type="password" class="form-control" id="inputPassword3" placeholder="Password" />
-          </div>
-        </div>
-        <button class="btn btn-primary inloggen" href="/catalogus">Inloggen</button>
+        <button class = "btn btn-primary inloggen" href = "/catalogus">
+          Inloggen
+        </button>
       </div>
-      <div class="GeenAccount">
+      <div class = "GeenAccount">
         <p>Nog geen account? =</p>
-        <button class="btn btn-light" href="/accountAanmaken">Account aanmaken</button>
+        <button class = "btn btn-light" href = "/accountAanmaken">
+          Account aanmaken
+        </button>
       </div>
     </div>
   </div>
 
-  <script type="module" src=""></script>
-  <script src="/bootstrap/js/bootstrap.js"></script>
+  <script type = "module" src = ""></script>
+  <script src = "/bootstrap/js/bootstrap.js"></script>
 </body>
 
 </html>
