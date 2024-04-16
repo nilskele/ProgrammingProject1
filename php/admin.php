f0a91aef01cb2d6bd925c2a014aeec8b1a8e20dc
nils.kelecom <kelecom.nils@gmail.com>
2024-04-16 14:52:40 +0200

    Admin page

--- php/admin.php
+++ php/admin.php
@@ -11,11 +11,23 @@
                 <h1>Product Nr.</h1>
                 <input type="text" class="form-control" id="inputName3" placeholder="product nr" />
             </div>
+            <div class="knoppen">
+                <a href="accepteren.php" class = "btn btn-light">acceptere</a>
+                <a href="defect.php" class = "btn btn-light">defect</a>
+                <a href="reserveren.php" class = "btn btn-light">reserveren</a>
+                
+            </div>
         </div>
         <div class="adminAccepteren">
             <div class="productNr">
                 <h1>Product Nr.</h1>
                 <input type="text" class="form-control" id="inputName3" placeholder="product nr" />
+            </div>
+            <div class="knoppen">
+                <a href="accepteren.php" class = "btn btn-light">acceptere</a>
+                <a href="defect.php" class = "btn btn-light">defect</a>
+                <a href="reserveren.php" class = "btn btn-light">reserveren</a>
+                
             </div>
         </div>
         <div class="adminAccepteren"></div>
