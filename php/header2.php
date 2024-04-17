0a45d69ad5c50ca9e7d0258e94e0882efdfde266
nils.kelecom <kelecom.nils@gmail.com>
2024-04-17 20:07:35 +0200

    search bar in/out page

--- php/header2.php
+++ php/header2.php
@@ -1,14 +1,20 @@
 
-<nav class="navbar navbar-light">
-  <a class="navbar-brand mb-0 h1 medialab" href="#">
-    <img src="../images/EhB-logo-transparant.png" width="60" class="d-inline-block align-top"
-      alt="ehb EhB-logo-transparant" />
-    <span class="medialabTitleNav">Medialab</span>
+<nav class = "navbar navbar-light nav_shadow">
+  <a class = "navbar-brand mb-0 h1 medialab" href = "#">
+    <img  src = "../images/EhB-logo-transparant.png" 
+          width = "60" 
+          class = "d-inline-block align-top"
+          alt = "ehb EhB-logo-transparant" />
+    <span class = "medialabTitleNav">
+      Medialab
+    </span>
   </a>
-  <nav class="navbar navbar-expand-lg navbar-light">
-    <div class="navbar-nav">
-      <a class="nav-item nav-link" href="inandout.php">In and Out</a>
-      <a class="nav-item nav-link" href="blacklist.php">Blacklist</a>
-    </div>
-  </nav>
+  <div class = "navbar-nav">
+  <a class="nav-item nav-link" href="inandout.php">In and Out</a>
+        
+    </a>
+    <a class="nav-item nav-link" href="blacklist.php">Blacklist</a>
+       
+    </a>
+  </div>
 </nav>
