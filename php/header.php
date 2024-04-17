e56b3152715375365852103f35bd3537d08f34d9
gustberchmans <gustberchmans@gmail.com>
2024-04-17 20:44:57 +0200

    header shadow fix

--- php/header.php
+++ php/header.php
@@ -1,6 +1,6 @@
 
-<nav class = "navbar navbar-light">
-  <a class = "navbar-brand mb-0 h1 medialab" href = "#">
+<nav class = "navbar navbar-light nav_shadow">
+  <a class = "navbar-brand mb-0 h1 medialab" href = "index.php">
     <img  src = "../images/EhB-logo-transparant.png" 
           width = "60" 
           class = "d-inline-block align-top"
