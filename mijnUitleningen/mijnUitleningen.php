dbd69096303c5613b15240741b71bb952ab1bb69
gustberchmans <gustberchmans@gmail.com>
2024-04-18 12:27:46 +0200

    invh

--- mijnUitleningen/mijnUitleningen.php
+++ mijnUitleningen/mijnUitleningen.php
@@ -5,7 +5,7 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Catalogus</title>
-  <link rel = "stylesheet" href = "../mijnLeningen/mijnUitleningen.css" />
+  <link rel = "stylesheet" href = "../mijnUitleningen/mijnUitleningen.css" />
   <link rel = "stylesheet" href = "../bootstrap/css/bootstrap.min.css" />
   <link rel = "stylesheet" href = "../css/styles.css" />
   
