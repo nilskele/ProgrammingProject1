5d3364e094b40431ef2668d9f2f6fcb20371619e
Vretkabas <90098564+Vretkabas@users.noreply.github.com>
2024-04-18 18:07:47 +0200

    upload deel admin kalender HTML en CSS

--- php/admin.php
+++ php/admin.php
@@ -44,4 +44,42 @@
         </div>
         <div class = "adminAccepteren"></div>
     </div>
+   
 </div>
+  <!-- container voor kalender -->
+  <div class="container3"> 
+  <div class="hoofd_kalender">
+    <div class="titel_kalender">
+      <h1>Kalender</h1>
+      <h2>Canon M50</h2>
+    </div>
+  </div>
+  <div class="buttons_kalender">
+    <form action="/zoeken" method="GET">
+      <input type="text" name="Zoeken" placeholder="Zoeken...">
+    </form>
+    <div class="buttons-container">
+      <a href="/kit_toevoegen"><button>Kit toevoegen</button></a>
+      <a href="/product_toevoegen"><button>Product toevoegen</button></a>
+    </div>
+  </div>
+
+  <div class="calender">
+  <div class="header">
+  <button class="arrow_prev"><img src="../images/black_arrow_l.png" alt="Previous"></button>
+    <h2>WEEK DATUM HIER</h2>
+    <button class="arrow_next"><img src="../images/black_arrow_r.png" alt="Next"></button>
+  </div>
+  <div class="dagen">
+    <div class="dag">Monday</div>
+    <div class="dag">Tuesday</div>
+    <div class="dag">Wednesday</div>
+    <div class="dag">Thursday</div>
+    <div class="dag">Friday</div>
+    <div class="dag">Saturday</div>
+    <div class="dag">Sunday</div>
+  </div>
+</div>
+
+</div>
+
