de3ff89fa3227d544a9108ca11ae15ff287855ac
gustberchmans <gustberchmans@gmail.com>
2024-04-23 15:10:41 +0200

    Update product_toevoegen.php

--- php/product_toevoegen.php
+++ php/product_toevoegen.php
@@ -24,13 +24,6 @@
         <option value="1">1</option>
         <option value="2">2</option>
         <option value="3">3</option>
-        <option value="4">4</option>
-        <option value="5">5</option>
-        <option value="6">6</option>
-        <option value="7">7</option>
-        <option value="8">8</option>
-        <option value="9">9</option>
-        <option value="10">10</option>
         </select><br>
         <label for="beschrijving">Beschrijving:</label>
         <input type="text" id="beschrijving" name="beschrijving"><br>
