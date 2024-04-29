37f6b66ff1661391f69911ede877a6f4dbc69d8d
jente_tav <54480755+jente01t@users.noreply.github.com>
2024-04-29 10:12:54 +0200

    admin login gemaakt

--- js/login.js
+++ js/login.js
@@ -26,3 +26,41 @@ document.getElementById("loginForm").addEventListener("submit", function (event)
         event.preventDefault();
     }
 });
+
+
+function OpenAdminLogin() {
+    document.getElementById("AdminLoginDiv").style.display = "block";
+}
+
+function CloseAdminLogin() {
+    document.getElementById("AdminLoginDiv").style.display = "none";
+}
+
+document.getElementById("AdminLoginForm").addEventListener("submit", function (event) {
+    let email = document.getElementById("inputEmail3").value;
+    let password = document.getElementById("inputPassword3").value;
+
+    if (email.trim() === "" && password.trim() === "") {
+        Swal.fire({
+            icon: "error",
+            title: "Fout",
+            text: "Gelieve alle velden in te vullen.",
+        });
+        event.preventDefault();
+    } else if (email.trim() === "") {
+        Swal.fire({
+            icon: "error",
+            title: "Fout",
+            text: "Gelieve een e-mailadres in te vullen.",
+        });
+        event.preventDefault();
+
+    } else if (password.trim() === "") {
+        Swal.fire({
+            icon: "error",
+            title: "Fout",
+            text: "Gelieve een wachtwoord in te vullen.",
+        });
+        event.preventDefault();
+    }
+});
--- php/index.php
+++ php/index.php
@@ -42,9 +42,33 @@
       <div class="Div3D">
         <img src="../images/3d-printer_44218.png" height="400px" alt="3d" />
       </div>
-      <a class="btn btn-primary adminbtn" href="admin.php">
+      <button class="btn btn-primary adminbtn" onclick="OpenAdminLogin()">
         Admin
-      </a>
+      </button>
+
+      <div class="adminLogin" id="AdminLoginDiv">
+        <form action="adminLogin.php" class="AdminLoginContainer" id="AdminLoginForm" method="POST">
+          <h1>Admin Login</h1>
+
+          <div class="form-group row">
+            <label for="inputEmail3" class="col-sm-2 col-form-label">Email:</label>
+            <div class="col-sm-10">
+              <input type="email" class="form-control" id="inputEmail3" name="inputEmail3" placeholder="Email">
+            </div>
+          </div>
+
+          <div class="form-group row">
+            <label for="inputPassword3" class="col-sm-2 col-form-label">Password:</label>
+            <div class="col-sm-10">
+              <input type="password" class="form-control" id="inputPassword3" name="inputPassword3" placeholder="Password">
+            </div>
+          </div>
+          <button type="submit" class="btn btn-primary inloggen">Inloggen</button>
+          <button type="button" class="btn btn-light" class="btn" onclick="CloseAdminLogin()">Terug</button>
+        </form>
+      </div>
+
+
       <form action="login.php" class="loginForm" id="loginForm" method="POST">
         <div class="form-group row">
           <label for="inputEmail3" class="col-sm-2 col-form-label">Email:</label>
