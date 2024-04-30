1986f0ab799b008f7d1e61950bcee16d1299db12
Vretkabas <90098564+Vretkabas@users.noreply.github.com>
2024-04-30 14:18:36 +0200

    veranderen van week met arrows

--- php/admin.index.php
+++ php/admin.index.php
@@ -92,9 +92,9 @@ include('../database.php');
 
   <div class="calender">
     <div class="header_kalender">
-      <button class="arrow_prev"><img src="../images/black_arrow_l.png" alt="Previous"></button>
+      <button class="arrow_prev" onclick="aanpassenWeek(-1)"><img src="../images/black_arrow_l.png" alt="Previous"></button>
       <h2>WEEK DATUM HIER</h2>
-      <button class="arrow_next"><img src="../images/black_arrow_r.png" alt="Next"></button>
+      <button class="arrow_next" onclick="aanpassenWeek(1)"><img src="../images/black_arrow_r.png" alt="Next"></button>
     </div>
     <div class="container_kalender">
 
@@ -112,35 +112,18 @@ $query_naamItem = "SELECT naam FROM GROEP";
 $result_naamItem = $conn->query($query_naamItem);
 
 
-//jaar en week van vandaag berekenen
-// Datum instellen (huidige datum)
-$datum = date('Y-m-d');
 
-// Eerste dag van september dit jaar
-$eerste_september = date('Y-m-01', strtotime($datum));
-
-// Dag van de week voor 1 september
-$weekdag_eerste_september = date('N', strtotime($eerste_september));
-
-// Als 1 september niet op maandag (1) valt, gaan we naar de volgende maandag
-if ($weekdag_eerste_september != 1) {
-    $eerste_september = date('Y-m-d', strtotime($eerste_september . ' next Monday'));
-}
-
-// Datum van de derde maandag van september
-$derde_maandag = date('Y-m-d', strtotime($eerste_september . ' +2 weeks'));
-
-// Weeknummer berekenen
-$weeknummer = date('W', strtotime($derde_maandag));
-
-echo "We zitten in weeknummer: $weeknummer";
-
-$year = 2024; 
+$year = isset($_GET['year']) ? $_GET['year'] : 2024;
+$week = isset($_GET['week']) ? $_GET['week'] : 26;
 
 
 // Calculate the start and end date of the week
-$start_date = date("Y-m-d", strtotime($year."W".$weeknummer));
-$end_date = date("Y-m-d", strtotime($year."W".$weeknummer."+6 days"));
+$start_date = date("Y-m-d", strtotime($year."W".$week));
+$end_date = date("Y-m-d", strtotime($year."W".$week."+6 days"));
+
+//start datum en eind datum zonder jaar
+$startDateMaand = date("d/m", strtotime($year."W".$week));
+$endDateMaand = date("d/m", strtotime($year."W".$week."+6 days"));
 
 
 // Start the table and center it
@@ -149,7 +132,7 @@ echo '<table border="1" style="table-layout: fixed; width: 80%; margin: 0 auto; 
 
 // Print header row for days of the week
 echo '<tr>';
-echo '<th style="width: 12%; border: 1px solid black;">Week '.$weeknummer.', '.$year.'</th>'; // New column for product names
+echo '<th style="width: 12%; border: 1px solid black;">'.$startDateMaand.' - '.$endDateMaand.'</th>';  // New column for product names
 for ($day = strtotime($start_date); $day <= strtotime($end_date); $day = strtotime("+1 day", $day)) {
     echo '<td style="width: 12%; padding: 10px; border: 1px solid black;">'; // Add border to the cell
     echo date("D j/n", $day);
@@ -211,3 +194,29 @@ for ($day = strtotime($start_date); $day <= strtotime($end_date); $day = strtoti
 echo '</table>';
 echo '</div>';
 ?>
+
+
+<script> 
+//functie aanmaken 
+function aanpassenWeek(wissel){
+  var currentYear = <?php echo $year; ?>;
+    var currentWeek = <?php echo $week; ?>;
+    
+    // Calculate the new week
+    var newWeek = currentWeek + wissel;
+    var newYear = currentYear;
+
+    if (newWeek > 52) {
+      newWeek = 1;
+      newYear++;
+    } else if (newWeek < 1) {
+      newWeek = 52;
+      newYear--;
+    }
+
+    // Redirect to the new week
+    window.location.href = "admin.index.php?year=" + newYear + "&week=" + newWeek; // Change "your_page.php" to your actual page
+  }
+
+
+</script>
