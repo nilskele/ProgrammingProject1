94395326c8bc017f5ecc9036fcf81d2de1fcd2e1
jente_tav <54480755+jente01t@users.noreply.github.com>
2024-05-07 13:42:06 +0200

    defect knop in accepteren

--- js/admin.index.js
+++ js/admin.index.js
@@ -87,17 +87,5 @@ defectBtn.addEventListener("click", function () {
         }    
 });
 
-document.addEventListener("DOMContentLoaded", function() {
-        $(document).on('click', '.defectBtn', function(event) {
-            event.preventDefault();
-            let itemText = document.getElementById('accepterenProductID').textContent;
-            let itemValue = itemText.match(/\d+/);
-            if (itemValue) {
-                localStorage.setItem("productNr", itemValue[0]);
-            } else {
-                console.log("No numerical value found.");
-            }
-            window.location.href = "admin.defectProduct.php";
-        });
-    });
+    
     
