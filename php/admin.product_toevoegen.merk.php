8ae8d08d56ed8b1cd5bd59fc49ee7622a4622d23
gustberchmans <gustberchmans@gmail.com>
2024-05-07 14:18:54 +0200

    sweet alerts staan nergens en nergens errors te vinden ma het werkt wel

--- php/admin.product_toevoegen.merk.php
+++ php/admin.product_toevoegen.merk.php
@@ -1,26 +1,26 @@
 <?php
-include('../database.php');
-include('validation_functions.php');
+include ('../database.php');
+include ('validation_functions.php');
 
-if(isset($_POST['merk'])) {
-    $merk = valideren($_POST['merk']);
+if (isset($_POST['merk'])) {
+  $merk = valideren($_POST['merk']);
 
-    $sql = "SELECT merk_id FROM MERK WHERE naam = ?";
-    
-    $stmt = $conn->prepare($sql);
-    $stmt->bind_param('s', $merk);
-    $stmt->execute();
+  $sql = "SELECT merk_id FROM MERK WHERE naam = ?";
 
-    $result = $stmt->get_result();
+  $stmt = $conn->prepare($sql);
+  $stmt->bind_param('s', $merk);
+  $stmt->execute();
 
-    if ($result->num_rows > 0) {
-        $row = $result->fetch_assoc();
-        echo json_encode(array('merk_id' => $row['merk_id'])); // Brand exists, return ID
-    } else {
-        echo json_encode(array('exists' => false)); // Brand does not exist
-    }
+  $result = $stmt->get_result();
+
+  if ($result->num_rows > 0) {
+    $row = $result->fetch_assoc();
+    echo json_encode(array('merk_id' => $row['merk_id'])); // Brand exists, return ID
+  } else {
+    echo json_encode(array('exists' => false)); // Brand does not exist
+  }
 } else {
-    echo 'Error: Brand not received.';
+  echo 'Error: Brand not received.';
 }
 
 $conn->close();
