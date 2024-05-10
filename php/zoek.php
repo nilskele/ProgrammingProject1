49b6d5f2b9f9ad10024aa17fc745d45446485ea4
Samia-almtlek <146224174+Samia-almtlek@users.noreply.github.com>
2024-05-10 23:41:13 +0200

    Update for reserveren btn en fetch data

--- php/zoek.php
+++ php/zoek.php
@@ -12,14 +12,14 @@ $zoekterm = isset($_GET['zoekbalk']) ? '%' . $_GET['zoekbalk'] . '%' : '%';
 $response = [];
 
 if ($zoekterm !== '%') {
-    $stmt = $conn->prepare("SELECT GROEP.naam AS groep_naam, MERK.naam AS merk_naam, PRODUCT.opmerkingen, BESCHRIJVING.naam AS beschrijving_naam, MIN(PRODUCT.datumBeschikbaar) AS datumBeschikbaar, COUNT(PRODUCT.product_id) AS aantal_beschikbare_producten, IMAGE.image_data
+    $stmt = $conn->prepare("SELECT GROEP.groep_id AS groep_id, GROEP.naam AS groep_naam, MERK.naam AS merk_naam, PRODUCT.opmerkingen, BESCHRIJVING.naam AS beschrijving_naam, MIN(PRODUCT.datumBeschikbaar) AS datumBeschikbaar, COUNT(PRODUCT.product_id) AS aantal_beschikbare_producten, IMAGE.image_data
     FROM GROEP
              INNER JOIN MERK ON GROEP.merk_id_fk = MERK.merk_id
              INNER JOIN PRODUCT ON GROEP.groep_id = PRODUCT.groep_id
              INNER JOIN BESCHRIJVING ON GROEP.beschrijving_id_fk = BESCHRIJVING.besch_id
              INNER JOIN IMAGE ON GROEP.image_id_fk =  IMAGE.image_id
     WHERE GROEP.naam LIKE ? AND PRODUCT.zichtbaar = true AND PRODUCT.isUitgeleend = false
-    GROUP BY GROEP.naam, MERK.naam, PRODUCT.opmerkingen, BESCHRIJVING.naam, IMAGE.image_data");
+    GROUP BY GROEP.groep_id, GROEP.naam, MERK.naam, PRODUCT.opmerkingen, BESCHRIJVING.naam, IMAGE.image_data");
     $stmt->bind_param("s", $zoekterm);
 
     $stmt->execute();
@@ -38,14 +38,14 @@ if ($zoekterm !== '%') {
     $stmt->close();
     
 } else {
-    $stmt = $conn->prepare("SELECT GROEP.naam AS groep_naam, MERK.naam AS merk_naam, PRODUCT.opmerkingen, BESCHRIJVING.naam AS beschrijving_naam, MIN(PRODUCT.datumBeschikbaar) AS datumBeschikbaar, COUNT(PRODUCT.product_id) AS aantal_beschikbare_producten, IMAGE.image_data
+    $stmt = $conn->prepare("SELECT GROEP.groep_id AS groep_id, GROEP.naam AS groep_naam, MERK.naam AS merk_naam, PRODUCT.opmerkingen, BESCHRIJVING.naam AS beschrijving_naam, MIN(PRODUCT.datumBeschikbaar) AS datumBeschikbaar, COUNT(PRODUCT.product_id) AS aantal_beschikbare_producten, IMAGE.image_data
     FROM GROEP
              INNER JOIN MERK ON GROEP.merk_id_fk = MERK.merk_id
              INNER JOIN PRODUCT ON GROEP.groep_id = PRODUCT.groep_id
              INNER JOIN BESCHRIJVING ON GROEP.beschrijving_id_fk = BESCHRIJVING.besch_id
              INNER JOIN IMAGE ON GROEP.image_id_fk =  IMAGE.image_id
     WHERE PRODUCT.zichtbaar = true AND PRODUCT.isUitgeleend = false
-    GROUP BY GROEP.naam, MERK.naam, PRODUCT.opmerkingen, BESCHRIJVING.naam, IMAGE.image_data");
+    GROUP BY GROEP.groep_id, GROEP.naam, MERK.naam, PRODUCT.opmerkingen, BESCHRIJVING.naam, IMAGE.image_data");
 
     $stmt->execute();
 
