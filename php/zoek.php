be99eea2e4d1cffaf70dfd51c3bcb58359d56d46
jente_tav <54480755+jente01t@users.noreply.github.com>
2024-05-12 17:30:04 +0200

    Kit: functie om kit te laten tonen in de catalogus

--- php/zoek.php
+++ php/zoek.php
@@ -12,14 +12,41 @@ $zoekterm = isset($_GET['zoekbalk']) ? '%' . $_GET['zoekbalk'] . '%' : '%';
 $response = [];
 
 if ($zoekterm !== '%') {
-    $stmt = $conn->prepare("SELECT GROEP.groep_id AS groep_id, GROEP.naam AS groep_naam, MERK.naam AS merk_naam, PRODUCT.opmerkingen, BESCHRIJVING.naam AS beschrijving_naam, MIN(PRODUCT.datumBeschikbaar) AS datumBeschikbaar, COUNT(PRODUCT.product_id) AS aantal_beschikbare_producten, IMAGE.image_data
-    FROM GROEP
-             INNER JOIN MERK ON GROEP.merk_id_fk = MERK.merk_id
-             INNER JOIN PRODUCT ON GROEP.groep_id = PRODUCT.groep_id
-             INNER JOIN BESCHRIJVING ON GROEP.beschrijving_id_fk = BESCHRIJVING.besch_id
-             INNER JOIN IMAGE ON GROEP.image_id_fk =  IMAGE.image_id
-    WHERE GROEP.naam LIKE ? AND PRODUCT.zichtbaar = true AND PRODUCT.isUitgeleend = false
-    GROUP BY GROEP.groep_id, GROEP.naam, MERK.naam, PRODUCT.opmerkingen, BESCHRIJVING.naam, IMAGE.image_data");
+    $stmt = $conn->prepare("SELECT GROEP.groep_id AS groep_id, GROEP.naam AS groep_naam, MERK.naam AS merk_naam, PRODUCT.opmerkingen, BESCHRIJVING.naam AS beschrijving_naam, MIN(PRODUCT.datumBeschikbaar) AS datumBeschikbaar,
+    COUNT(PRODUCT.product_id) AS aantal_beschikbare_producten, IMAGE.image_data
+FROM GROEP
+      INNER JOIN MERK ON GROEP.merk_id_fk = MERK.merk_id
+      INNER JOIN PRODUCT ON GROEP.groep_id = PRODUCT.groep_id
+      INNER JOIN BESCHRIJVING ON GROEP.beschrijving_id_fk = BESCHRIJVING.besch_id
+      INNER JOIN IMAGE ON GROEP.image_id_fk =  IMAGE.image_id
+WHERE GROEP.naam LIKE ? AND PRODUCT.zichtbaar = true AND PRODUCT.isUitgeleend = false
+GROUP BY GROEP.groep_id, GROEP.naam, MERK.naam, PRODUCT.opmerkingen, BESCHRIJVING.naam, IMAGE.image_data
+
+UNION
+
+SELECT KIT.kit_id, KIT.kit_naam, MERK.naam AS merk_naam, KIT.opmerkingen, GROUP_CONCAT(GROEP.naam) AS beschrijving_naam, MIN(KIT.datumBeschikbaar) AS datumBeschikbaar ,
+    (SELECT COUNT(*) FROM PRODUCT JOIN GROEP ON PRODUCT.groep_id = GROEP.groep_id where PRODUCT.isUitgeleend = false and PRODUCT.zichtbaar AND PRODUCT.groep_id = GROEP.groep_id GROUP BY product_id limit 1) AS aantal_beschikbare_producten,
+    IMAGE.image_data
+FROM KIT
+      JOIN CATEGORY ON KIT.category_fk = CATEGORY.cat_id
+      JOIN MERK ON KIT.merk_fk = MERK.merk_id
+      LEFT JOIN KIT_PRODUCT ON KIT.kit_id = KIT_PRODUCT.kit_id_fk
+      LEFT JOIN GROEP ON KIT_PRODUCT.groep_id_fk = GROEP.groep_id
+      LEFT JOIN IMAGE ON KIT.image_id_fk = IMAGE.image_id
+WHERE KIT.isUitgeleend = false
+AND KIT.zichtbaar = true
+AND EXISTS (
+ SELECT 1 FROM PRODUCT
+ WHERE PRODUCT.groep_id = GROEP.groep_id
+   AND PRODUCT.isUitgeleend = false
+   AND PRODUCT.zichtbaar = true
+)
+GROUP BY KIT.kit_id, KIT.kit_naam, MERK.naam, KIT.opmerkingen, IMAGE.image_data
+HAVING COUNT(DISTINCT KIT_PRODUCT.groep_id_fk) >= (
+ SELECT COUNT(*)
+ FROM KIT_PRODUCT
+ WHERE KIT_PRODUCT.kit_id_fk = KIT.kit_id
+);");
     $stmt->bind_param("s", $zoekterm);
 
     $stmt->execute();
@@ -38,14 +65,41 @@ if ($zoekterm !== '%') {
     $stmt->close();
     
 } else {
-    $stmt = $conn->prepare("SELECT GROEP.groep_id AS groep_id, GROEP.naam AS groep_naam, MERK.naam AS merk_naam, PRODUCT.opmerkingen, BESCHRIJVING.naam AS beschrijving_naam, MIN(PRODUCT.datumBeschikbaar) AS datumBeschikbaar, COUNT(PRODUCT.product_id) AS aantal_beschikbare_producten, IMAGE.image_data
-    FROM GROEP
-             INNER JOIN MERK ON GROEP.merk_id_fk = MERK.merk_id
-             INNER JOIN PRODUCT ON GROEP.groep_id = PRODUCT.groep_id
-             INNER JOIN BESCHRIJVING ON GROEP.beschrijving_id_fk = BESCHRIJVING.besch_id
-             INNER JOIN IMAGE ON GROEP.image_id_fk =  IMAGE.image_id
-    WHERE PRODUCT.zichtbaar = true AND PRODUCT.isUitgeleend = false
-    GROUP BY GROEP.groep_id, GROEP.naam, MERK.naam, PRODUCT.opmerkingen, BESCHRIJVING.naam, IMAGE.image_data");
+    $stmt = $conn->prepare("SELECT GROEP.groep_id AS groep_id, GROEP.naam AS groep_naam, MERK.naam AS merk_naam, PRODUCT.opmerkingen, BESCHRIJVING.naam AS beschrijving_naam, MIN(PRODUCT.datumBeschikbaar) AS datumBeschikbaar,
+    COUNT(PRODUCT.product_id) AS aantal_beschikbare_producten, IMAGE.image_data
+FROM GROEP
+      INNER JOIN MERK ON GROEP.merk_id_fk = MERK.merk_id
+      INNER JOIN PRODUCT ON GROEP.groep_id = PRODUCT.groep_id
+      INNER JOIN BESCHRIJVING ON GROEP.beschrijving_id_fk = BESCHRIJVING.besch_id
+      INNER JOIN IMAGE ON GROEP.image_id_fk =  IMAGE.image_id
+WHERE PRODUCT.zichtbaar = true AND PRODUCT.isUitgeleend = false
+GROUP BY GROEP.groep_id, GROEP.naam, MERK.naam, PRODUCT.opmerkingen, BESCHRIJVING.naam, IMAGE.image_data
+
+UNION
+
+SELECT KIT.kit_id, KIT.kit_naam, MERK.naam AS merk_naam, KIT.opmerkingen, GROUP_CONCAT(GROEP.naam) AS beschrijving_naam, MIN(KIT.datumBeschikbaar) AS datumBeschikbaar ,
+    (SELECT COUNT(*) FROM PRODUCT JOIN GROEP ON PRODUCT.groep_id = GROEP.groep_id where PRODUCT.isUitgeleend = false and PRODUCT.zichtbaar AND PRODUCT.groep_id = GROEP.groep_id GROUP BY product_id limit 1) AS aantal_beschikbare_producten,
+    IMAGE.image_data
+FROM KIT
+      JOIN CATEGORY ON KIT.category_fk = CATEGORY.cat_id
+      JOIN MERK ON KIT.merk_fk = MERK.merk_id
+      LEFT JOIN KIT_PRODUCT ON KIT.kit_id = KIT_PRODUCT.kit_id_fk
+      LEFT JOIN GROEP ON KIT_PRODUCT.groep_id_fk = GROEP.groep_id
+      LEFT JOIN IMAGE ON KIT.image_id_fk = IMAGE.image_id
+WHERE KIT.isUitgeleend = false
+AND KIT.zichtbaar = true
+AND EXISTS (
+ SELECT 1 FROM PRODUCT
+ WHERE PRODUCT.groep_id = GROEP.groep_id
+   AND PRODUCT.isUitgeleend = false
+   AND PRODUCT.zichtbaar = true
+)
+GROUP BY KIT.kit_id, KIT.kit_naam, MERK.naam, KIT.opmerkingen, IMAGE.image_data
+HAVING COUNT(DISTINCT KIT_PRODUCT.groep_id_fk) >= (
+ SELECT COUNT(*)
+ FROM KIT_PRODUCT
+ WHERE KIT_PRODUCT.kit_id_fk = KIT.kit_id
+);");
 
     $stmt->execute();
 
