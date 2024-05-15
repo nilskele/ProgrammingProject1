d3e356612a1b863e21574025eb99931c2badb2eb
gustberchmans <gustberchmans@gmail.com>
2024-05-15 15:24:45 +0200

    Update admin.index.php
    dubbele css weggehaald

--- php/admin/admin.index.php
+++ php/admin/admin.index.php
@@ -102,8 +102,6 @@ include('../../database.php');
   <meta charset="UTF-8" />
   <meta http-equiv="X-UA-Compatible" content="IE=edge" />
   <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
   <script src="https://cdn.jsdelivr.net/npm/@simondmc/popup-js@1.4.3/popup.min.js"></script>
   <script src="catalogus/getData.php"></script> 
     <script src="catalogus/kalender.backend.js"></script> 
