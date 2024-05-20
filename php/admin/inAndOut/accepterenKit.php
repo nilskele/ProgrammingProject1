220f0c62d0168bf57a43de6788c21ad80b8fe17d
jente_tav <54480755+jente01t@users.noreply.github.com>
2024-05-20 12:47:30 +0200

    Kit: admin kit accepteren frontend

---
+++ php/admin/inAndOut/accepterenKit.php
@@ -0,0 +1,29 @@
+<link rel="stylesheet" href="css/accepterenKit.css" />
+
+<?php 
+include("../admin.header.php");
+?>
+
+<div class="container">
+    <div class="accepterenKitContainer">
+        <div class="titelContainer">
+            <div class="inforPersoonDiv">
+                <p><strong> Geleend door: </strong> <span id="naamPersoon">Jente Tavernier</span></p>
+                <p><strong> Email: </strong> <span id="emailPersoon">jente.tavernier@student.ehb.be</span></p>
+            </div>
+            <div class="naamKit">
+                <h1 class="naamKKitTitel"><span id="naamKit">Canon Kit</span></h1>
+                <h1 class="idKitTitel"><span id="idKit">1</span></h1>
+            </div>
+        </div>
+        <div class="accepterenContainer">
+            <div class="productenLijst">
+                
+            </div>
+        </div>
+    </div>
+    <div class="accepterenFooter">
+            <button type="button" class="btn btn-primary accepterenBtnKit">Kit Accepteren</button>
+            <p class="aantalProductenBinnen">2/4 producten zijn geaccepteerd</p>
+        </div> 
+</div>
