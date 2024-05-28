0d221e183ff198007039a82308e16a4c23839223
Samia-almtlek <146224174+Samia-almtlek@users.noreply.github.com>
2024-05-28 14:57:20 +0200

    Update for code to fix the (reden) and make it kort

--- php/admin/statistieke/statistiekePagina.js
+++ php/admin/statistieke/statistiekePagina.js
@@ -1,57 +1,43 @@
 function filterProducts() {
     var groupID = document.getElementById('searchInput').value;
-    if (groupID === "") {
-        // Als het zoekveld leeg is, haal dan alle gegevens op
-        loadAllProducts();
-        return;
+    var url = 'statistiekeBackendPagina.php';
+    if (groupID !== "") {
+        url += '?groupID=' + groupID;
     }
+    fetchData(url);
+}
+
+function loadAllProducts() {
+    fetchData('statistiekeBackendPagina.php');
+}
+
+function fetchData(url) {
     var xhr = new XMLHttpRequest();
-    xhr.open('GET', 'statistiekeBackendPagina.php?groupID='+ groupID, true);
+    xhr.open('GET', url, true);
     xhr.onload = function() {
         if (xhr.status === 200) {
             var data = JSON.parse(xhr.responseText);
-            var table = document.getElementById('productTable');
-            table.innerHTML = `
-                <tr>
-                    <th>Groep_id</th>
-                    <th>Product naam</th>
-                    <th>Aantal keer gereserveerd</th>
-                    <th>Aantal defecten gemeld</th>
-                    <th>De meest voorkomende reden</th>
-                </tr>`;
-            data.forEach(function(row) {
-                var tr = document.createElement('tr');
-                tr.innerHTML = `<td>${row.groep_id}</td><td>${row.naam}</td><td>${row.reserve_count}</td><td>${row.defect_count}</td><td>${row.most_common_reason}</td>`;
-                table.appendChild(tr);
-            });
+            displayProducts(data);
         }
     };
     xhr.send();
 }
 
-function loadAllProducts() {
-    var xhr = new XMLHttpRequest();
-    xhr.open('GET', 'statistiekeBackendPagina.php', true);
-    xhr.onload = function() {
-        if (xhr.status === 200) {
-            var data = JSON.parse(xhr.responseText);
-            var table = document.getElementById('productTable');
-            table.innerHTML = `
-                <tr>
-                    <th>Groep_id</th>
-                    <th>Product naam</th>
-                    <th>Aantal keer gereserveerd</th>
-                    <th>Aantal defecten gemeld</th>
-                    <th>De meest voorkomende reden</th>
-                </tr>`;
-            data.forEach(function(row) {
-                var tr = document.createElement('tr');
-                tr.innerHTML = `<td>${row.groep_id}</td><td>${row.naam}</td><td>${row.reserve_count}</td><td>${row.defect_count}</td><td>${row.most_common_reason}</td>`;
-                table.appendChild(tr);
-            });
-        }
-    };
-    xhr.send();
+function displayProducts(data) {
+    var table = document.getElementById('productTable');
+    table.innerHTML = `
+        <tr>
+            <th>Groep_id</th>
+            <th>Product naam</th>
+            <th>Aantal keer gereserveerd</th>
+            <th>Aantal defecten gemeld</th>
+            <th>De meest voorkomende reden</th>
+        </tr>`;
+    data.forEach(function(row) {
+        var tr = document.createElement('tr');
+        tr.innerHTML = `<td>${row.groep_id}</td><td>${row.naam}</td><td>${row.reserve_count}</td><td>${row.defect_count}</td><td>${row.most_common_reason}</td>`;
+        table.appendChild(tr);
+    });
 }
 
 // Laad alle producten bij het laden van de pagina
