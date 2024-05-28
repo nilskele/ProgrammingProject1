efc86da0367c3555e6601d456147d58135c36201
nils.kelecom <kelecom.nils@gmail.com>
2024-05-28 12:01:40 +0200

    defect pagina: kleine UI verandering.

--- php/admin/defect/js/defect.js
+++ php/admin/defect/js/defect.js
@@ -26,16 +26,18 @@ $(function() {
                         <div id="vandaagInButtons">
                              <h6>${item.naam}</h6>
                             <img class="statusImage1" src="data:image/jpeg;base64,${item.image_data}">
+
                         </div>
                         <div class="info">
                         <p>${item.voornaam} ${item.achternaam}</p>
                         <p class="accepterenProductID"  value="${item.email}">${item.email}</p>
-                        <p class="productNr"  value="${item.product_id}"> Product Nr:${item.product_id}</p>
                         </div>
                         <div class="info">
                             <p class="Naam" value="">wat is er kapot: ${item.watDefect}</p>
                             <p class="accepterenProductID" value="">hoe is het kapot: ${item.redenDefect}</p>
                         </div>
+                        <p class="productNr"  value="${item.product_id}"> Product Nr:${item.product_id}</p>
+
                     `;
     
                     card.appendChild(productInfo);
